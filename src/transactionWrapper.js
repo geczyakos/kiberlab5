834475c8ff4c8580886283277d8c82f6b1fbd44c
geczyakos <geczyakos@gmail.com>
2026-08-19 10:04:37 +0200

    feat: add transaction wrapper

---
+++ src/transactionWrapper.js
@@ -0,0 +1,11 @@
+export async function withTransaction(client, fn) {
+  try {
+    await client.query('BEGIN');
+    const result = await fn(client);
+    await client.query('COMMIT');
+    return result;
+  } catch (err) {
+    await client.query('ROLLBACK');
+    throw err;
+  }
+}
